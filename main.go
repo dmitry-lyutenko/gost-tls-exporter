2ddfcac57e882bb400cdd2179c1bd0524036aacf
Dmitry Lyutenko <ldatook@mail.ru>
2026-06-01 18:26:50 +0300

    Added metrics, README

--- main.go
+++ main.go
@@ -58,6 +58,11 @@ func main() {
 
 	metricsCache := &sync.Map{}
 
+	http.HandleFunc("/healthz", func(w http.ResponseWriter, r *http.Request) {
+		w.WriteHeader(http.StatusOK)
+		w.Write([]byte("OK"))
+	})
+
 	http.HandleFunc("/probe", returnProbeHandler(metricsCache, cacheTTL))
 	http.Handle("/metrics", promhttp.Handler())
 
@@ -70,7 +75,11 @@ func main() {
 	term := make(chan os.Signal, 1)
 	signal.Notify(term, os.Interrupt, syscall.SIGTERM)
 
-	slog.Info("exporter started", "version", version, "address", *listenAddress, "port", *listenPort, "cache_ttl", cacheTTL.String())
+	if level == slog.LevelDebug {
+		slog.Debug("gost-tls-exporter started", "version", version, "address", *listenAddress, "port", *listenPort, "cache_ttl", cacheTTL.String())
+	} else {
+		slog.Info("gost-tls-exporter started", "version", version, "address", *listenAddress, "port", *listenPort, "cache_ttl", cacheTTL.String())
+	}
 	go func() {
 
 		if err := srv.ListenAndServe(); err != nil && err != http.ErrServerClosed {
@@ -82,7 +91,6 @@ func main() {
 	for {
 		select {
 		case <-term:
-			slog.Info("shutting down gracefully")
 			// Создаем контекст с таймаутом, чтобы не ждать вечно
 			ctx, cancel := context.WithTimeout(context.Background(), 15*time.Second)
 			// Shutdown плавно закрывает все соединения и останавливает сервер
